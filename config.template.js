cd416700bb42894082afb8cdb4c4bfa07777c772
SunburntRock89 <SunburntRock89@gmail.com>
2020-11-19 23:31:52 +0000

    Upload the actual bot

---
+++ config.template.js
@@ -0,0 +1,6 @@
+module.exports = {
+	discordToken: "",
+}
+// Invite URL: https://discord.com/api/oauth2/authorize?client_id={client_ID}&permissions=126016&scope=bot
+// Your own comicsansbold.fnt must be supplied
+// you can convert your TTF to this using https://github.com/libgdx/libgdx/wiki/Hiero
